f48f2f7c36545383871031cb9408fac153457bf1
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2026-03-03 00:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+let currentAudio = null;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    const soundName = button.innerText;
+    if (button.classList.contains("stop")) {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      return;
+    }
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
